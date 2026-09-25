554e99954ea15198735d8db0465635e134e58f71
Vansh Gupta <vanshgupta9151@gmail.com>
2026-09-25 11:10:03 +0530

    Add deployment files

--- app.py
+++ app.py
@@ -1525,7 +1525,7 @@ if __name__ == "__main__":
     print("=" * 60)
 
     app.run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000)),
-        debug=False
+        debug=True,
+        host="127.0.0.1",
+        port=5000
     )
